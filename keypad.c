8d4ed9ea4420ecfee2ae635f08200774ed3bcb25
jasmine2000 <jasmine2000@gmail.com>
2022-05-07 23:04:12 -0400

    more modes

--- keypad.c
+++ keypad.c
@@ -4,9 +4,11 @@
 
 extern const int max_players;
 
-const int COLUMN_SELECT = 0; // gameplay
-const int PLAYER_SELECT = 1; // adding/deleting during setup
-const int OPTION_SELECT = 2; // choosing options during setup
+extern const int COLUMN_SELECT; // gameplay
+extern const int PLAYER_SELECT; // adding/deleting during setup
+extern const int OPTION_SELECT; // choosing options during setup
+extern const int CONFIRM_SELECT;    // confirming a choice
+extern const int GAME_SELECT;       // game type
 
 const int keypad_map[16] = {
     1,  2,  3,  10, 
@@ -19,36 +21,52 @@ int get_keypress(int mode, int next) {
 
     int data_available;
     int keypad_index;
-    int keypad_number;
+    int key;
 
     for (;;) {   
         data_available = data_available_Read();
         if (data_available == 1) {
             keypad_index = A_Read() + (B_Read() << 1) + (C_Read() << 2) + (D_Read() << 3);
+            break;
         }
     }
 
-    keypad_number = keypad_map[keypad_index];
+    key = keypad_map[keypad_index];
     
-    if (mode == COLUMN_SELECT) {
-        if (keypad_number >= 1 && keypad_number <= 7) {
-            return keypad_number;
+    if (mode == COLUMN_SELECT) {            // 1-7, D, *
+        if ((key >= 1 && key <= 7) || key == 13 || key == 20) {
+            return key;
         } else {
             return 0;
         }
 
-    } else if (mode == PLAYER_SELECT) {
-        if (keypad_number > 0 && keypad_number <= next) {
-            return keypad_number;
+    } else if (mode == PLAYER_SELECT) {     // 1-next
+        if (key > 0 && key <= next) {
+            return key;
         } else {
             return 0;
         }
 
-    } else if (mode == OPTION_SELECT) {
-        if (keypad_number == 1 || keypad_number == 2 || (keypad_number >= 10 && keypad_number <= 12)) {
-            return keypad_number;
+    } else if (mode == OPTION_SELECT) {     // 0-2, A-C, *
+        if (key == 1 || key == 2 || (key >= 10 && key <= 12) || key == 20 || key == 30) {
+            return key;
+        } else {
+            return 0;
+        }
+
+    } else if (mode == CONFIRM_SELECT) {    // C only
+        if (key == 12) {
+            return 1;
+        } else {
+            return 0;
+        }
+
+    } else if (mode == GAME_SELECT) {       // 1 or 2
+        if (key == 1 || key == 2) {
+            return key;
         } else {
             return 0;
         }
     }
+    return 0;
 }
